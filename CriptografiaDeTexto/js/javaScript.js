4dfc1b8611b40d7f492e5787fa3248a5edeb71fe
Elton Frota Cavalcante de Sousa <frota4@yahoo.com.br>
2022-02-14 18:11:54 -0300

    tentativa

--- CriptografiaDeTexto/js/javaScript.js
+++ CriptografiaDeTexto/js/javaScript.js
@@ -1,70 +1,85 @@
 
-botaocriptografia () {
-    //essa var recebe o que foi escrito dentro da label  
+/*
+function criptografia() { // Abre a função Criptografia
+   //essa var recebe o que foi escrito dentro da label  
 
-   var capturaInput = document.getElementById("btn-cripto").value;
-    criptografa = capturaInput;
- 
-    var armazenaCriptografia = criptografa ;
-    var mapObj ={
-       e:"enter",
-       i:"imes",
-       a:"ai",
-       o:"ober",
-       u:"ufat"
-    };
+   var capturaInput = document.getElementById("texto").value;
+   criptografa = capturaInput;
+
    
-    armazenaCriptografia = armazenaCriptografia.replace(/e|i|a|o|u/gi, function(matched){
-       return mapObj[matched]
-      
-    });
-    
-    armazenaMsg(armazenaCriptografia);
- 
-  }  
- 
- 
-  function botaodescriptografia (){
-    var capturaInput = document.getElementById("armazenainput").value;
-    var descriptografa = capturaInput;
- 
-    var armazenaDescriptografia = descriptografa ;
-    var mapObj ={
-       enter:"e",
-       imes:"i",
-       ai:"a",
-       ober:"o",
-       ufat:"u"};
-   
-    armazenaDescriptografia = armazenaDescriptografia.replace(/enter|imes|ai|ober|ufat/gi, function(matched){
-       return mapObj[matched]
-    });  
-  
-    armazenaMsg(armazenaDescriptografia);
-    
-  } //fechamento da function
- 
-  
- function armazenaMsg(armazenatxt){
-    var msg = document.getElementById("recebeMensagem");
-     msg.innerHTML = (armazenatxt);
- 
- 
-  
- }
- 
- function copiarTexto() {
-    let textoCopiado = document.getElementById("recebeMensagem");
-    textoCopiado.select();
-    textoCopiado.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-    
-    // cria uma mensagem de copiado
-    Swal.fire(
-       'Copiado com sucesso!',
-       "Obrigado !",
-       "success"  
-       
-       )
- }
-btn-cripto.addEventListener("click", botaodescriptografia);
+   var mapObj = {
+      e: "enter",
+      i: "imes",
+      a: "ai",
+      o: "ober",
+      u: "ufat"
+   };
+
+   armazenaCriptografia = armazenaCriptografia.replace(/e|i|a|o|u/gi, function (matched) {
+      return mapObj[matched]
+
+   });
+
+   armazenaMsg(armazenaCriptografia);
+
+}// Fecha a função Criptografia
+
+
+function descriptografia() { // Abre a função otaoDescriptografia
+   var capturaInput = document.getElementById("texto").value;
+   var descriptografa = capturaInput;
+
+   var armazenaDescriptografia = descriptografa;
+   var mapObj = {
+      enter: "e",
+      imes: "i",
+      ai: "a",
+      ober: "o",
+      ufat: "u"
+   };
+
+   armazenaDescriptografia = armazenaDescriptografia.replace(/enter|imes|ai|ober|ufat/gi, function (matched) {
+      return mapObj[matched]
+   });
+
+   armazenaMsg(armazenaDescriptografia);
+
+} //fechamento da função botaoDescriptografia
+
+
+function armazenaMsg(armazenatxt) {
+   var msg = document.getElementById("textoPraCript");
+   msg.innerHTML = (armazenatxt);
+}
+
+function copiarTexto() { // Inicio do função Copiar Texto
+   let textoCopiado = document.getElementById("textoPraCript");
+   textoCopiado.select();
+   textoCopiado.setSelectionRange(0, 99999)
+   document.execCommand("copy");
+
+   // cria uma mensagem de copiado
+   Swal.fire(
+      'Copiado com sucesso!',
+      "Obrigado !",
+      "success"
+   )
+} // Fim do função Copiar Texto
+
+btncripto.addEventListener("click", botaodescriptografia);
+*/
+var segredos = [5,7,10,2];
+
+var input = document.querySelector("textarea");
+input.focus();
+
+var contador = 0;
+function verifica() {
+   for(var posicao; contador < segredos.length; contador++) {
+      if(input.value == segredos[posicao]) {
+         alert("Você ACERTOU!");
+      } 
+   }
+}
+var botao = document.querySelector("input");
+botao.onclick = verifica;
